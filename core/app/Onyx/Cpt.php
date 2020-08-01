990832438934045c325846dccbafec0fdb34cc5f
Andre Macola <andremacola@gmail.com>
2020-08-01 06:18:31 -0300

    automatic set custom capabilities

--- core/app/Onyx/Cpt.php
+++ core/app/Onyx/Cpt.php
@@ -148,7 +148,7 @@ class Cpt {
 	}
 
 	/**
-	 * Set custom capability for post type
+	 * Set or create custom capability for post type
 	 *
 	 * @param string $capability
 	 * @return void
@@ -160,11 +160,34 @@ class Cpt {
 	/**
 	 * Set custom capabilities for post type
 	 *
-	 * @param array $capabilities
+	 * @param array|bool $capabilities
 	 * @return void
 	 */
-	public function capabilities( $capabilities = [] ) {
-		$this->capabilities = $capabilities;
+	public function capabilities( $capabilities = false ) {
+		if ( is_array( $capabilities ) && ! empty( $capabilities ) ) {
+			$this->capabilities = $capabilities;
+		} elseif ( is_string( $capabilities ) && 'auto' === $capabilities ) {
+			$singular = sanitize_title( $this->name );
+			$plural   = sanitize_title( $this->plural );
+
+			$this->capabilities = [
+				'edit_post'              => "edit_{$singular}",
+				'read_post'              => "read_{$singular}",
+				'delete_post'            => "delete_{$singular}",
+				'edit_posts'             => "edit_{$plural}",
+				'edit_others_posts'      => "edit_others_{$plural}",
+				'publish_posts'          => "publish_{$plural}",
+				'read_private_posts'     => "read_private_{$plural}",
+				'create_posts'           => "create_{$plural}",
+				'read'                   => "read_{$plural}",
+				'delete_posts'           => "delete_{$plural}",
+				'delete_private_posts'   => "delete_private_{$plural}",
+				'delete_published_posts' => "delete_published_{$plural}",
+				'delete_others_posts'    => "delete_others_{$plural}",
+				'edit_private_posts'     => "edit_private_{$plural}",
+				'edit_published_posts'   => "edit_published_{$plural}",
+			];
+		}
 	}
 
 	/**
@@ -267,9 +290,13 @@ class Cpt {
 		}
 
 		// set capability.
+		if ( isset( $arguments['custom_caps'] ) ) {
+			$this->capabilities( 'auto' );
+		}
+
 		if ( ! isset( $arguments['capabilities'] ) && ! empty( $this->capabilities ) ) {
 			$arguments['capabilities'] = $this->capabilities;
-			$arguments['map_meta_cap'] = ( ! empty( $this->arguments['map_meta_cap'] )) ? $this->arguments['map_meta_cap'] : true;
+			$arguments['map_meta_cap'] = ( isset( $this->arguments['map_meta_cap'] ) ) ? $this->arguments['map_meta_cap'] : true;
 		}
 
 		// create and set labels.
