bb1a31bcad49a1054406ecb6579119a0e7e3b809
andremacola <andremacola@gmail.com>
2024-10-02 17:33:07 -0300

    disable fullscreen mode by default

--- src/js/admin/gutenberg.js
+++ src/js/admin/gutenberg.js
@@ -7,4 +7,10 @@ wp.domReady(function() {
 	// remove discussion panel
 	const { removeEditorPanel } = wp.data.dispatch('core/edit-post');
 	removeEditorPanel('discussion-panel');
+
+	// disable fullscreen mode by default
+	const isFullscreenMode = wp.data.select('core/edit-post').isFeatureActive('fullscreenMode');
+	if (isFullscreenMode) {
+		wp.data.dispatch('core/edit-post').toggleFeature('fullscreenMode');
+	}
 });
