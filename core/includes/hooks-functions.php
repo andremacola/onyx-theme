5c7fff4e08de95a9bb86ccdc9a0a6b0c7e8fd154
andremacola <andremacola@gmail.com>
2024-10-16 15:30:21 -0300

    fix warning: onyx_disable_comments

--- core/includes/hooks-functions.php
+++ core/includes/hooks-functions.php
@@ -424,10 +424,27 @@ add_action( 'wp_dashboard_setup', 'onyx_dashboard_widgets' );
  * @return void
  */
 function onyx_disable_comments_trackbacks() {
-	global $pagenow;
-	$post_types = get_post_types();
+	global $post_types;
 
-	remove_menu_page( 'edit-comments.php' );
+	if ( is_array( $post_types ) ) {
+		foreach ( $post_types as $post_type ) {
+			if ( post_type_supports( $post_type, 'comments' ) ) {
+				remove_post_type_support( $post_type, 'comments' );
+				remove_post_type_support( $post_type, 'trackbacks' );
+			}
+		}
+	}
+
+	add_action('admin_menu',
+		function() {
+			global $pagenow;
+			remove_menu_page( 'edit-comments.php' );
+			if ( 'edit-comments.php' === $pagenow ) {
+				wp_safe_redirect( admin_url() );
+				exit;
+			}
+		}
+	);
 
 	add_action('wp_before_admin_bar_render',
 		function() {
@@ -435,20 +452,8 @@ function onyx_disable_comments_trackbacks() {
 			$wp_admin_bar->remove_menu( 'comments' );
 		}
 	);
-
-	if ( 'edit-comments.php' === $pagenow ) {
-		wp_safe_redirect( admin_url() );
-		exit;
-	}
-
-	foreach ( $post_types as $post_type ) {
-		if ( post_type_supports( $post_type, 'comments' ) ) {
-			remove_post_type_support( $post_type, 'comments' );
-			remove_post_type_support( $post_type, 'trackbacks' );
-		}
-	}
 }
-// add_action( 'admin_bar_init', 'onyx_disable_comments_trackbacks' );
+// add_action( 'after_setup_theme', 'onyx_disable_comments_trackbacks' );
 
 /**
  * Filter allowed mime types to upload.
